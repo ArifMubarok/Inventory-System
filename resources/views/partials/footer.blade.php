06287cfc5725ef708f1f438324376e42931c929c
Ahmad Sufian <ahmadfian360@student.uns.ac.id>
2022-04-14 10:46:10 +0700

    UI dashboard2

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,10 +4,10 @@
   </aside>
   <!-- /.control-sidebar -->
 
-<footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
+<footer class="main-footer text-center">
+    <strong>Copyright &copy; {{ date("Y") }} <a href="/dashboard">msInventaris</a></strong>
+    Develope by Faust
+    {{-- <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 3.2.0
-    </div>
+    </div> --}}
 </footer>
