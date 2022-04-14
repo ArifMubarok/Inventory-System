e5f0cd806f627da816341399108fa5cbbeb58b15
Ahmad Sufian <ahmadfian360@student.uns.ac.id>
2022-04-14 10:51:05 +0700

    UI dashboard2

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,8 +5,7 @@
   <!-- /.control-sidebar -->
 
 <footer class="main-footer text-center">
-    <strong>Copyright &copy; {{ date("Y") }} <a href="/dashboard">msInventaris</a></strong>
-    Develope by Faust
+    <strong>Copyright &copy; {{ date("Y") }} <a href="/dashboard">msInventaris</a></strong> Develope by CV. Cipta Mandiri Solusindo | Faust
     {{-- <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 3.2.0
     </div> --}}
