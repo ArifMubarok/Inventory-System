0a1e30a4a06a89c9fd475c000bb64b18c8a0db58
Ahmad Hasan Ali <>
2022-07-04 13:52:46 +0700

    Riwayat Laporan, minor update
    minor update = tanggal pengadaan in data pengadaan dalam detail barang format tanggal dirubah

--- resources/views/pages/admin/barang/barang/detail_barang/isi_menu/riwayat_laporan.blade.php
+++ resources/views/pages/admin/barang/barang/detail_barang/isi_menu/riwayat_laporan.blade.php
@@ -1,3 +1,44 @@
+@foreach ($riwayat_laporan as $rl)
+@php
+$cek = $rl->laporan
+@endphp
+@endforeach
+@if (isset($cek))
+<div class="row">
+<div class="col-md-12">
+    <div class="form-group row m-b-15">
+        <h3>Riwayat Laporan</h3>
+        <div class="table-responsive panel-body m-4">
+            <table class="table table-striped table-bordered m-b-0">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Judul Laporan</th>
+                        <th>Laporan</th>
+                        <th>Status</th>
+                        <th>Dilaporkan Oleh</th>
+                        <th>Tgl Laporan</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($riwayat_laporan as $r_laporan)
+                        <tr>
+                            {{-- @dump($r_laporan) --}}
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $r_laporan->judul_laporan }}</td>
+                            <td>{{ $r_laporan->laporan }}</td>
+                            <td>{{ $r_laporan->status }}</td>
+                            <td>{{ $r_laporan->pelapor }}</td>
+                            <td>{{ $r_laporan->created_at->format('d M Y') }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@else
 <div class="row">
     <div class="col-md-12">
         <div class="form-group row m-b-15">
@@ -13,3 +54,4 @@
     </div>
 </div>
 {{-- end row --}}
+@endif
--- resources/views/pages/admin/cek-barang/detail_barang/isi_menu/riwayat_laporan.blade.php
+++ resources/views/pages/admin/cek-barang/detail_barang/isi_menu/riwayat_laporan.blade.php
@@ -1,15 +1,60 @@
+@foreach ($riwayat_laporan as $rl)
+@php
+$cek = $rl->laporan
+@endphp
+@endforeach
+@if (isset($cek))
 <div class="row">
     <div class="col-md-12">
         <div class="form-group row m-b-15">
-            <h3 class="pull-left">Riwayat Laporan</h3>
+            <h3>Riwayat Laporan</h3>
+            <div class="table-responsive panel-body m-4">
+                <table class="table table-striped table-bordered m-b-0">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Judul Laporan</th>
+                            <th>Laporan</th>
+                            <th>Status</th>
+                            <th>Dilaporkan Oleh</th>
+                            <th>Tgl Laporan</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($riwayat_laporan as $r_laporan)
+                            <tr>
+                                {{-- @dump($r_laporan) --}}
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $r_laporan->judul_laporan }}</td>
+                                <td>{{ $r_laporan->laporan }}</td>
+                                <td>{{ $r_laporan->status }}</td>
+                                <td>{{ $r_laporan->pelapor }}</td>
+                                <td>{{ $r_laporan->created_at->format('d M Y') }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-
-{{-- begin row --}}
-<div class="row">
-    <div class="col">
-        Belum ada data.
+    @else
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group row m-b-15">
+                <h3 class="pull-left">Riwayat Laporan</h3>
+            </div>
+        </div>
     </div>
-</div>
-{{-- end row --}}
+    
+    {{-- begin row --}}
+    <div class="row">
+        <div class="col">
+            Belum ada data.
+        </div>
+    </div>
+    {{-- end row --}}
+    @endif
+    
+    
+    
